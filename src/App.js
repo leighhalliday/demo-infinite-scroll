cc4aa78fc29606487337def2d3bbee7299a9520b
Leigh Halliday <leighhalliday@gmail.com>
2019-09-11 10:51:54 -0400

    Working on demo for infinite scrolling using intersection observer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const allData = [];
+for (let i = 1; i <= 25; i++) {
+  allData.push(i);
+}
+const perPage = 10;
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "start":
+      return { ...state, loading: true };
+    case "load":
+      const newData = allData.slice(state.after, state.after + perPage);
+      return {
+        ...state,
+        loading: false,
+        more: newData.length === perPage,
+        after: state.after + perPage,
+        data: [...state.data, ...newData]
+      };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, {
+    data: [],
+    after: 0,
+    loading: false,
+    more: true
+  });
+  const { data, loading, more } = state;
+  const [ref, setRef] = React.useState(null);
+  const observer = React.useRef(
+    new IntersectionObserver(
+      entries => {
+        if (!entries[0].isIntersecting) {
+          return;
+        }
+        dispatch({ type: "start" });
+        dispatch({ type: "load" });
+      },
+      { threshold: 1.0 }
+    )
+  );
+
+  React.useEffect(() => {
+    const currentRef = ref;
+    const currentObserver = observer.current;
+    if (currentRef) {
+      currentObserver.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        currentObserver.unobserve(currentRef);
+      }
+    };
+  }, [ref]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.map(row => (
+          <li key={row} style={{ height: "50vh", background: "orange" }}>
+            {row}
+          </li>
+        ))}
+      </ul>
+
+      {loading && <div>Loading...</div>}
+
+      {!loading && more && (
+        <div ref={setRef} style={{ height: "200px", background: "yellow" }}>
+          load more
+        </div>
+      )}
     </div>
   );
 }
